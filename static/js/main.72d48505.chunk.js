f1a166cd0f98989abad05d5e75480d83fd576f24
Yuliia Danylenko <yvdanilenko@gmail.com>
2021-12-17 01:02:11 +0200

    Updates

--- static/js/main.72d48505.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{3:function(e,o,t){"use strict";t.r(o);var n=t(0),c=t.n(n),r=t(2),s=t.n(r),a=c.a.createElement("div",{a:5,b:10},"hello");console.log(a),s.a.render(a,document.querySelector("#root"))}},[[3,1,2]]]);
-//# sourceMappingURL=main.72d48505.chunk.js.map
